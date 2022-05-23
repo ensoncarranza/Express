2a247cdbd7b5434d805c6486e10a0901fa3eef0c
ensoncarranza <91918775+ensoncarranza@users.noreply.github.com>
2022-05-22 23:05:56 -0600

    Update usersData.js

--- appExpress/usersData.js
+++ appExpress/usersData.js
@@ -1,8 +1,8 @@
 const data = [
     {
-        fullName: "Jose Gonzalez",
-        email: "jg@gmail.com",
-        password: ""
+        fullName: "Enson Carranza",
+        email: "EC@gmail.com",
+        password: "holamundo"
     }
 ];
 
